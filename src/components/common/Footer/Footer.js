5a610d91d1e820911a3834adbc3dcb18c57df1cc
Just Nutritive <alexise@justnutritive.com>
2019-07-22 14:20:00 -0400

    finished

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -3,10 +3,20 @@ import './footer.scss';
 
 const Footer = () => {
     return (
-        <footer className="w-100 text-center bg-dark text-white">
+        <footer className="w-100 text-center bg-dark text-white py-2">
+            <div className="card mx-auto">
+                <div className="card-body bg-dark text-white text-left">
+                    <p> Framework: ReactJs </p>
+                    <p> Concepts: state, props, prop-types, react-bootstrap.</p>
+                    <p> Api: https://newsapi.org</p>
+
+                </div>
+
+            </div>
+            
             <p>News App { new Date().getFullYear() }</p>
         </footer>
     )
 }
 
-export default Footer
+export default Footer;
